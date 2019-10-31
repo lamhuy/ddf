9e7ebe1092f3ffd8858dd2e9918638cdaa76439d
Zachary Adam <zta6@pitt.edu>
2019-10-31 11:33:20 -0600

    [13.x] DDF-5513 Fix anyGeo bounding box not updating correctly (#250)
    * DDF-5513 Fix anyGeo bbox updating incorrectly
    
    * fix test
    
    * remove unused var

--- ui/packages/catalog-ui-search/src/main/webapp/component/location-old/location-serialization.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/location-old/location-serialization.js
@@ -163,15 +163,11 @@ const Polygon = {
 const BoundingBox = {
   'json->location': json => {
     const {
-      geometry: { coordinates },
       properties: { north, east, south, west },
     } = json
 
-    const [polygon] = coordinates
-
     return {
       mode: 'bbox',
-      polygon,
       north,
       east,
       south,
